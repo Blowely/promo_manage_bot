35181c3fdfa625fb181ff70ee0abcb13aaab8602
Blowely <maryashin.2014@yandex.ru>
2023-01-04 02:43:22 +0300

    changed creds

--- src/db.js
+++ src/db.js
@@ -1,3 +1,3 @@
 const {Sequelize} = require('sequelize');
 
-module.exports = new Sequelize('moviefokl7', 'moviefokl7', 'Dfkz1947', {host: 'pg2.sweb.ru', port: '5432', dialect: "postgres"})
+module.exports = new Sequelize('moviefokl8', 'moviefokl8', 'Dfkz1947', {host: 'pg2.sweb.ru', port: '5432', dialect: "postgres"})
